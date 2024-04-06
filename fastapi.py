af41dd52a757a9d5107bb2ae24702291370a3b4a
locolemat <122878505+locolemat@users.noreply.github.com>
2024-04-06 16:39:45 +0800

    Added config module to fastapi

--- fastapi.py
+++ fastapi.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from telebot.async_telebot import AsyncTeleBot
 from robokassa import check_signature_result
+from user_configs import read_config, update_config
 
 import yaml
 import sqlite3
